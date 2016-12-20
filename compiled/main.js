c0824c435cddc83ab5a4ce64b465b454aad19c66
Niels Kristian Hansen Skovmand <nkhs@statsbiblioteket.dk>
2016-12-20 09:57:58 +0100

    Finished

--- compiled/main.js
+++ compiled/main.js
@@ -1,4 +1,13 @@
-var countdownTimerDiv = document.querySelector("#countdowntimer");
+var countdownTimerDiv = document.querySelector("#timer");
+var quick5 = document.querySelector(".box1");
+var quick10 = document.querySelector(".box2");
+var focus30 = document.querySelector(".box3");
+var focus60 = document.querySelector(".box4");
+var tabletennis = document.querySelector(".box5");
+var other = document.querySelector(".box6");
+var customtext = document.querySelector("#customtext");
+var customlength = document.querySelector("#customlength");
+var runningTimer;
 function timer(seconds) {
     var now = Date.now();
     var then = now + seconds * 1000;
@@ -11,12 +20,34 @@ function timer(seconds) {
             return;
         }
     }, 1000);
+    return timerInterval;
 }
 function displayTimeLeft(seconds) {
     var minutes = (seconds - (seconds % 60)) / 60;
     var remainderSeconds = seconds % 60;
     var display = minutes + ":" + (remainderSeconds < 10 ? '0' : '') + remainderSeconds;
     document.title = display;
-    if (countdownTimerDiv !== null)
-        countdownTimerDiv.textContent = display;
+    countdownTimerDiv.textContent = display;
 }
+function registerTimer(seconds) {
+    clearInterval(runningTimer);
+    runningTimer = timer(seconds);
+}
+quick5.addEventListener('click', function () { return registerTimer(5 * 60); });
+quick10.addEventListener('click', function () { return registerTimer(10 * 60); });
+focus30.addEventListener('click', function () { return registerTimer(30 * 60); });
+focus60.addEventListener('click', function () { return registerTimer(60 * 60); });
+tabletennis.addEventListener('click', function () { return registerTimer(15 * 60); });
+other.addEventListener('mouseover', function () {
+    customlength.style.display = "inline";
+    customtext.style.display = "none";
+});
+other.addEventListener('mouseleave', function () {
+    customlength.style.display = "none";
+    customtext.style.display = "inline";
+});
+customlength.addEventListener('keyup', function (event) {
+    if (event.keyCode === 13) {
+        registerTimer(parseInt(customlength.value));
+    }
+});
--- main.ts
+++ main.ts
@@ -1,4 +1,14 @@
-const countdownTimerDiv = document.querySelector("#countdowntimer");
+const countdownTimerDiv = document.querySelector("#timer");
+const quick5 = document.querySelector(".box1");
+const quick10 = document.querySelector(".box2");
+const focus30 = document.querySelector(".box3");
+const focus60 = document.querySelector(".box4");
+const tabletennis = document.querySelector(".box5");
+const other = document.querySelector(".box6");
+const customtext = document.querySelector("#customtext");
+const customlength = document.querySelector("#customlength");
+
+let runningTimer: number;
 
 function timer(seconds: number) {
     const now = Date.now();
@@ -14,6 +24,8 @@ function timer(seconds: number) {
             return;
         }
     }, 1000);
+
+    return timerInterval;
 }
 
 function displayTimeLeft(seconds: number) {
@@ -21,7 +33,33 @@ function displayTimeLeft(seconds: number) {
     const remainderSeconds = seconds % 60;
     const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`
     document.title = display;
-
-    if (countdownTimerDiv !== null)
-        countdownTimerDiv.textContent = display;
+    countdownTimerDiv.textContent = display;
 }
+
+function registerTimer(seconds: number) {
+    clearInterval(runningTimer)
+    runningTimer = timer(seconds)
+}
+
+quick5.addEventListener('click', () => registerTimer(5 * 60));
+quick10.addEventListener('click', () => registerTimer(10 * 60));
+focus30.addEventListener('click', () => registerTimer(30 * 60));
+focus60.addEventListener('click', () => registerTimer(60 * 60));
+tabletennis.addEventListener('click', () => registerTimer(15 * 60));
+
+other.addEventListener('mouseover', () => {
+    (<HTMLElement> customlength).style.display = "inline";
+    (<HTMLElement> customtext).style.display = "none";
+});
+
+other.addEventListener('mouseleave', () => {
+    (<HTMLElement> customlength).style.display = "none";
+    (<HTMLElement> customtext).style.display = "inline";
+})
+
+customlength.addEventListener('keyup', (event: Event) => {
+    if ((<KeyboardEvent>event).keyCode === 13) {
+        registerTimer(parseInt((<HTMLInputElement>customlength).value));
+    }
+})
+
